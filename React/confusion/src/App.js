70b02752e5e4d71d7f032127cdf7da85e2905cb3
ydubernet <yoadube@laposte.net>
2020-05-30 17:18:57 +0200

    First React app + Configuring React

---
+++ React/confusion/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar dark color="primary">
+      <div className="container">
+        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+      </div>
+    </Navbar>
+  </div>
+  );
+}
+
+export default App;
