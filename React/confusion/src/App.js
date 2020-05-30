207b66b881f4c566015bd795ea8013437dc65819
ydubernet <yoadube@laposte.net>
2020-05-30 17:31:02 +0200

    Components Part 1

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <Navbar dark color="primary">
       <div className="container">
         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        <Menu />
       </div>
     </Navbar>
   </div>
