302ba7f8cb795725094cd709b473bdc4c7533b68
ydubernet <yoadube@laposte.net>
2020-06-28 22:23:30 +0200

    React Router

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { BrowserRouter } from 'react-router-dom';
+
 import Main from './components/MainComponent';
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- React/confusion/src/components/FooterComponent.js
+++ React/confusion/src/components/FooterComponent.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer(props) {
     return(
     <div className="footer">
+        <li><Link to='/home'>Home</Link></li>
+        <li><Link to='/aboutus'>About Us</Link></li>
+        <li><Link to='/menu'>Menu</Link></li>
+        <li><Link to='/contactus'>Contact Us</Link></li>
+
+
         <div className="container">
             <div className="row justify-content-center">             
                 <div className="col-4 offset-1 col-sm-2">
