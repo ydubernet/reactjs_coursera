ca04dd71413428efd84ba8a418a258b1f15f61d6
ydubernet <yoadube@laposte.net>
2020-06-29 23:23:40 +0200

    React exam week 2

--- React/confusion/src/components/FooterComponent.js
+++ React/confusion/src/components/FooterComponent.js
@@ -4,8 +4,6 @@ import { Link } from 'react-router-dom';
 function Footer(props) {
     return(
     <div className="footer">
-
-
         <div className="container">
             <div className="row justify-content-center">             
                 <div className="col-4 offset-1 col-sm-2">
